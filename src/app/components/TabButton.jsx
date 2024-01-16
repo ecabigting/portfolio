4b019a64eaf5268fba5fa0eb053a8431ce8fe9b3
ecabigting <ericcabigting@outlook.com>
2024-01-16 17:26:21 +0400

    oops forgot the file

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const TabButton = ({ active, selectTab, children }) => {
-	const buttonClasses = active ? "text-white border-purple-500 border-b" : "text-[#ADB7BE]";
+const TabButton = ({ active, selectTab, childen }) => {
+	const buttonClasses = active ? "text-white border-b border-orange-500" : "text-[#ADB7BE]";
 
 	return (
 		<button onClick={selectTab}>
-			<p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>{children}</p>
+			<p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>{childen}</p>
 		</button>
 	);
 };
+
 export default TabButton;
