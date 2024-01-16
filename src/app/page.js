381fc86119074b62327b86e0245cae018614e40b
ecabigting <ericcabigting@outlook.com>
2024-01-16 14:05:47 +0400

    remove container class to allow full weight

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] ">
       <Navbar/>
-      <div className="container mt-24 p-4 px-12 py-4">
+      <div className="mt-24 p-4 py-4">
           <HeroSection/>
       </div>
     </main>
