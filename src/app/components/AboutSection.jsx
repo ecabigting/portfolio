68e5ae471649f4483ac022ea5be69625e63c199d
ecabigting <ericcabigting@outlook.com>
2024-01-16 20:20:45 +0400

    - added tab button component - added data for tabs - completed about section finishing touches styles

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,7 +1,70 @@
 "use client";
 import React, { useState, useTransition } from "react";
 import TabButton from "./TabButton";
+import Link from "next/link";
 import Image from "next/image";
+
+const TAB_DATA = [
+	{
+		title: "Experience",
+		id: "experience",
+		content: (
+			<ul className='list-disc pl-2 '>
+				<li>
+					<h3 className='text-md font-bold'>Full Stack Web Developer (Jul 2021 - Present)</h3>
+					<p className='text-sm font-light'>
+						Digital Qube Marketing Consultancy (formerly eWavelength Digital Marketing)
+					</p>
+					<p className='text-xs font-extralight'>Abu Dhabi, United Arab Emirates</p>
+				</li>
+				<li>
+					<h3 className='text-md font-bold'>Senior Software Developer (Aug 2017 - Jun 2021)</h3>
+					<p className='text-sm font-light'>
+						Digital Qube Marketing Consultancy(formerly eWavelength Digital Marketing)
+					</p>
+					<p className='text-xs font-extralight'>Dubai, United Arab Emirates</p>
+				</li>
+				<li>
+					<h3 className='text-md font-bold'>Web Developer (Jun 2014 - Jul 2017)</h3>
+					<p className='text-sm font-light'>Al Dhafra Private Schools</p>
+					<p className='text-xs font-extralight'>Abu Dhabi, United Arab Emirates</p>
+				</li>
+				<li className='text-right last:list-none'>
+					<Link href='#' className='text-md font-extralight'>
+						.. read more
+					</Link>
+				</li>
+			</ul>
+		),
+	},
+	{
+		title: "Skills",
+		id: "skills",
+		content: (
+			<ul className='list-disc pl-2'>
+				<li>C#, .net</li>
+				<li>HTML, JS, CSS</li>
+				<li>Rest API</li>
+				<li>NodeJS</li>
+				<li>React, NextJS, Angular</li>
+				<li>MSSQL, MySQL, MongoDB,</li>
+			</ul>
+		),
+	},
+	{
+		title: "Education",
+		id: "education",
+		content: (
+			<ul className='list-disc pl-2'>
+				<li>
+					<h3 className='text-md font-black'>Bachelor of Science in Computer Science </h3>
+					<p className='text-sm font-bold'>Angeles University Foundation </p>
+					<p className='text-xs font-light'>Angeles City, Philippines</p>
+				</li>
+			</ul>
+		),
+	},
+];
 
 const AboutSection = () => {
 	const [tab, setTab] = useState("experience");
@@ -14,10 +77,10 @@ const AboutSection = () => {
 	};
 	return (
 		<section className='text-white'>
-			<div className='m-2 md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+			<div className='m-2 md:grid md:grid-cols-2 gap-4 items-center py-8 px-4 xl:gap-8 sm:py-16 xl:px-16'>
 				<Image src='/images/aboutme-image2.jpg' width={400} height={400} className='max-auto place-self-center' />
-				<div>
-					<h2 className='text-4xl font-bold text-white mt-4'>about me</h2>
+				<div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
+					<h2 className='text-4xl font-bold text-white mt-4 border-b-2 border-b-orange-500 mb-4'>about me</h2>
 					<p className='text-base md:text-lg'>
 						Do irure Lorem est veniam sunt velit aliquip dolor cupidatat cupidatat consectetur. Cillum commodo id
 						nostrud elit ipsum culpa nisi Lorem. Ea magna anim minim incididunt consequat officia consequat do. Non
@@ -29,10 +92,16 @@ const AboutSection = () => {
 							{" "}
 							Experience{" "}
 						</TabButton>
-						<span className=''>Experience</span>
-						<span className=''>Skills</span>
-						<span className=''>Education</span>
+						<TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
+							{" "}
+							Skills{" "}
+						</TabButton>
+						<TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>
+							{" "}
+							Education{" "}
+						</TabButton>
 					</div>
+					<div className='mt-8'>{TAB_DATA.find((t) => t.id === tab).content}</div>
 				</div>
 			</div>
 		</section>
--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const TabButton = ({ active, selectTab, childen }) => {
-	const buttonClasses = active ? "text-white border-b border-orange-500" : "text-[#ADB7BE]";
+const TabButton = ({ active, selectTab, children }) => {
+	const buttonClasses = active ? "text-white border-orange-500 border-b" : "text-[#ADB7BE]";
 
 	return (
 		<button onClick={selectTab}>
-			<p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>{childen}</p>
+			<p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>{children}</p>
 		</button>
 	);
 };
