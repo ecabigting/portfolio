6abc5981bd54d10b3730ab96fb1263d4d679e87f
ecabigting <ericcabigting@outlook.com>
2024-01-16 12:39:15 +0400

    finished the hero section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,28 +1,55 @@
+"use client";
 import React from "react";
 import Image from "next/image"
+import { TypeAnimation } from 'react-type-animation';
 
 export const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hey! I'm Eric</h1>
-          <p className="text-[#ADB7BE] text-lg mb-5 lg:text-xl">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center pr-3 sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-400 
+            via-white-400 to-yellow-400 mr-4 bg-white">Hey! I'm Eric!{" "}</span>
+            <br/>
+                <TypeAnimation
+                  sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                    'A Full-stack dev',
+                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                    'Lets code!',
+                    1000
+                  ]}
+                  wrapper="span"
+                  speed={50}
+                  repeat={Infinity}
+                />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Nostrud adipisicing reprehenderit dolore in incididunt eu et elit sunt non ea.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire me!</button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">Download CV!</button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full 
+            bg-gradient-to-br from-red-500 via-white-500 to-yellow-500 
+            mr-4 bg-white hover:bg-slate-200 text-white">
+              Hire me!
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full 
+            bg-gradient-to-br from-red-500 via-white-500 to-yellow-500 
+            mt-3 hover:bg-slate-800 text-white">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-6 py-3">Download CV!</span>
+            </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[300px] lg:h-[300px] relative mt-4">
+          <div className="rounded-full w-[250px] h-[250px] lg:w-[250px] lg:h-[250px] relative mt-4
+            bg-gradient-to-r from-red-400 via-white-400 to-yellow-400 bg-[#181818] ">
             <Image 
             src="/images/hero-image.png" 
             alt="Hero"
             className=" rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover"
-            width={300} 
-            height={300}
+            width={250} 
+            height={250}
             />
           </div>
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import { HeroSection } from './components/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto p-4 px-12 py-4">
-      <div className="container"><HeroSection/></div>
+    <main className="flex min-h-screen flex-col bg-[#121212] ">
+      <div className="container mx-auto p-4 px-12 py-4">
+          <HeroSection/>
+      </div>
     </main>
   )
 }
