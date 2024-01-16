49fbacd57c883e90521028e6be56a95064db4108
ecabigting <ericcabigting@outlook.com>
2024-01-16 14:25:35 +0400

    started about me section

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutSection = () => {
+  return (
+    <section className="text-white">
+      <div className >
+
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
