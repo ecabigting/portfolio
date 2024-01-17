7b66b6e5c84b3fc215225e6e7168f4c74e1cc49f
ecabigting <ericcabigting@outlook.com>
2024-01-17 17:19:56 +0400

    - added email

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,62 +1,67 @@
 "use client";
-import Link from 'next/link'
-import React,{useState} from 'react'
-import NavLink from './NavLink'
-import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid"
-import MenuOverlay from './MenuOverlay';
+import Link from "next/link";
+import React, { useState } from "react";
+import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
-    {
-        title: "about",
-        href: "#about"
-    },
-    {
-        title: "projects",
-        href: "#projects"
-    },
-    {
-        title: "contact",
-        href: "#contact"
-    }
-]
+	{
+		title: "about",
+		href: "#about",
+	},
+	{
+		title: "projects",
+		href: "#projects",
+	},
+	{
+		title: "contact",
+		href: "#contact",
+	},
+	{
+		title: "github",
+		href: "https://github.com/ecabigting",
+	},
+];
 
 const Navbar = () => {
-  const [navBarOpen, setNavbarOpen] = useState(false);
-  return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212]">
-        <div className="flex flex-wrap items-center justify-between mx-auto p-8 px-4">
-            <Link href={"/"} className="text-lg md:text-3xl text-white font-semibold">
-                ecabigting
-            </Link>
-            <div className="mobile-menu block md:hidden">
-                {
-                    !navBarOpen ? (
-                        <button onClick={()=>setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
-                            <Bars3Icon className="h-5 w-5"/>
-                        </button>
-                    ) : (
-                        <button onClick={()=>setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
-                            <XMarkIcon className="h-5 w-5"/>
-                        </button>
-                    )
-                }
-            </div>
-            <div className="menu hidden md:block md:w-auto" id="navbar">
-                <ul className="flex p4 md:p-0 md:flex-row md:space-x-8 mt-0">
-                    {
-                        navLinks.map((link,index) => (
-                                <li key={index}>
-                                    <NavLink href={link.href} title={link.title}/>
-                                </li>
-                            )
-                        )
-                    }
-                </ul>
-            </div>
-        </div>
-        { navBarOpen ? <MenuOverlay links={navLinks}/> : null}
-    </nav>
-  )
-}
+	const [navBarOpen, setNavbarOpen] = useState(false);
+	return (
+		<nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212]'>
+			<div className='flex flex-wrap items-center justify-between mx-auto p-8 px-4'>
+				<Link href={"/"} className='text-lg md:text-3xl text-white font-semibold'>
+					ecabigting
+				</Link>
+				<div className='mobile-menu block md:hidden'>
+					{!navBarOpen ? (
+						<button
+							onClick={() => setNavbarOpen(true)}
+							className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'
+						>
+							<Bars3Icon className='h-5 w-5' />
+						</button>
+					) : (
+						<button
+							onClick={() => setNavbarOpen(false)}
+							className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'
+						>
+							<XMarkIcon className='h-5 w-5' />
+						</button>
+					)}
+				</div>
+				<div className='menu hidden md:block md:w-auto' id='navbar'>
+					<ul className='flex p4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+						{navLinks.map((link, index) => (
+							<li key={index}>
+								<NavLink href={link.href} title={link.title} />
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+			{navBarOpen ? <MenuOverlay links={navLinks} /> : null}
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectData = [
 	{
@@ -7,7 +9,7 @@ const projectData = [
 		title: "Wild Wadi Waterpark™",
 		description: "E-commerce website for the Wild Wadi Waterpark™",
 		image: "/images/projects/thumbs/proj-wildwadi.webp",
-		tag: ["ALL,WEB"],
+		tag: ["ALL", "WEB"],
 		gitHub: "",
 		projectURL: "https://wildwadi.com/",
 	},
@@ -16,7 +18,7 @@ const projectData = [
 		title: "Inside Burj Al Arab",
 		description: "E-commerce website for Inside Burj Al Arab Tours",
 		image: "/images/projects/thumbs/proj-ibaa.webp",
-		tag: ["ALL,WEB,MOBILE"],
+		tag: ["ALL", "WEB"],
 		gitHub: "",
 		projectURL: "https://insideburjalarab.com/",
 	},
@@ -25,7 +27,7 @@ const projectData = [
 		title: "The Original Barracuda",
 		description: "E-commerce and reward app for The Originall Barrcuda Bottle Shop",
 		image: "/images/projects/thumbs/proj-barracuda.webp",
-		tag: ["ALL,WEB,MOBILE"],
+		tag: ["ALL", "WEB", "MOBILE"],
 		gitHub: "",
 		projectURL: "https://www.originalbarracuda.com/",
 	},
@@ -34,7 +36,7 @@ const projectData = [
 		title: "MovieAPP",
 		description: "Website for searching movies using TheMovieDB API",
 		image: "/images/projects/thumbs/proj-movieapp.webp",
-		tag: ["ALL,WEB,MOBILE"],
+		tag: ["ALL", "WEB"],
 		gitHub: "https://github.com/ecabigting/movieapp",
 		projectURL: "https://ecabigting-movieapp.netlify.app/",
 	},
@@ -43,7 +45,7 @@ const projectData = [
 		title: "DCIAA-IIAN",
 		description: "Mobile app and dashboard investigation and Notification system for Dubai Civil Aviation Authority",
 		image: "/images/projects/thumbs/proj-dcaaiian.webp",
-		tag: ["ALL,WEB,MOBILE"],
+		tag: ["ALL", "MOBILE"],
 		gitHub: "",
 		projectURL: "https://apps.apple.com/us/app/dcaa-iian/id1466031139",
 	},
@@ -52,7 +54,7 @@ const projectData = [
 		title: "Al Mawashi",
 		description: "E-commerce and Mobile app with backend store management system for Al Mawashi Meat Shop in MENA",
 		image: "/images/projects/thumbs/proj-mawashi.webp",
-		tag: ["ALL,WEB,MOBILE"],
+		tag: ["ALL", "WEB", "MOBILE"],
 		gitHub: "",
 		projectURL: "https://www.almawashistore.com/",
 	},
@@ -61,7 +63,7 @@ const projectData = [
 		title: "Al Dhafra Private Schools",
 		description: "School Management Portal for Al Dhafra Private Schools",
 		image: "/images/projects/thumbs/proj-dhafra.webp",
-		tag: ["ALL,WEB,MOBILE"],
+		tag: ["ALL", "MOBILE"],
 		gitHub: "",
 		projectURL: "https://dhafraschools.com/",
 	},
@@ -70,17 +72,29 @@ const projectData = [
 		title: "nextSpot",
 		description: "Private online media sharing and review system for Fotokem Industries INC.",
 		image: "/images/projects/thumbs/proj-nextspot.webp",
-		tag: ["ALL,WEB,MOBILE"],
+		tag: ["ALL", "WEB"],
 		gitHub: "",
 		projectURL: "https://nextspot.fotokem.com/",
 	},
 ];
 const ProjectsSection = () => {
+	const [tag, setTag] = useState("ALL");
+	const handleTagChange = (newTag) => {
+		setTag(newTag);
+	};
+
+	const filteredProjects = projectData.filter((project) => project.tag.includes(tag));
+
 	return (
 		<>
 			<h2 className='text-center text-4xl font-bold text-white mt-4 mb-4'>Projects</h2>
+			<div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
+				<ProjectTag onClick={handleTagChange} name='ALL' isSelected={tag === "ALL"} />
+				<ProjectTag onClick={handleTagChange} name='WEB' isSelected={tag === "WEB"} />
+				<ProjectTag onClick={handleTagChange} name='MOBILE' isSelected={tag === "MOBILE"} />
+			</div>
 			<div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-				{projectData.map((project) => (
+				{filteredProjects.map((project) => (
 					<ProjectCard
 						key={project.id}
 						title={project.title}
