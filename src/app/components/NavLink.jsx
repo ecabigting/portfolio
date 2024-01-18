40d095ae0674acc9c26fd59bbfbb18d45ffa59d8
ecabigting <ericcabigting@outlook.com>
2024-01-18 15:14:37 +0400

    implement scroll to view

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,12 +1,25 @@
 import Link from "next/link";
 
 const NavLink = ({ href, title, hideMainOverLay }) => {
+	const handleOnClick = (e) => {
+		e.preventDefault();
+		const href = e.currentTarget.href;
+		const targetId = href.replace(/.*\#/, "");
+		const elem = document.getElementById(targetId);
+		elem?.scrollIntoView({
+			behavior: "smooth",
+		});
+		hideMainOverLay();
+	};
+
 	return (
 		<div>
 			<Link
 				href={href}
 				className='block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white'
-				onClick={hideMainOverLay}
+				onClick={(e) => {
+					handleOnClick(e);
+				}}
 			>
 				{title}
 			</Link>
