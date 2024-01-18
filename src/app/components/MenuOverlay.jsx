a4560eef339d448c3655cfc07a203302eb3db07d
ecabigting <ericcabigting@outlook.com>
2024-01-18 15:31:11 +0400

    - fix scrolling navigation  - Issue when using Link on anchor in netlify:  - Solution is to use button instead of link and manuall scroll using scrollIntoView()

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -6,7 +6,7 @@ const MenuOverlay = ({ links, hideMainOverlay }) => {
 		<ul className='flex flex-col py-4 items-center'>
 			{links.map((link, index) => (
 				<li key={index} className='border-b border-b-slate-50'>
-					<NavLink href={link.href} title={link.title} hideMainOverLay={hideMainOverlay} />
+					<NavLink target={link.href} title={link.title} hideMainOverLay={hideMainOverlay} />
 				</li>
 			))}
 		</ul>
