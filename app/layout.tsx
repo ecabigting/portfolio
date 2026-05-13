157a705bd051019a8f6e2008df08fbd684e00c2b
ecabigting <ericcabigting@outlook.com>
2026-05-13 20:35:51 +0400

    update
    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import { GoogleAnalytics } from '@next/third-parties/google';
 import Footer from "@/components/layout/FooterWrapper";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://ericcabigting.dev"),
